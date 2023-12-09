789a2cc7ac0ba56f350d8c39d19ace9fceee3497
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2023-12-09 01:14:31 -0500

    first java practice code

---
+++ Practice/src/practice/userInput.java
@@ -0,0 +1,13 @@
+package practice;
+import java.util.Scanner;
+public class userInput {
+	
+	public static void main(String[] args) {
+		
+		
+Scanner scan = new Scanner(System.in);
+System.out.print("What's your name? ");
+String input = scan.nextLine(); 
+System.out.println("Hello "+input);
+}
+}
