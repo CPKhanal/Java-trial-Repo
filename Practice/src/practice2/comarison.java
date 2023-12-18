f457a72ebb1d5bb81263b378983d9daa6efd4856
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2023-12-18 00:09:55 -0500

    checking if the number given by user is greater than 8 or not

---
+++ Practice/src/practice2/comarison.java
@@ -0,0 +1,17 @@
+package practice2;
+import java.util.Scanner;
+
+//checking if the number given by user is greater than 8 or not
+
+public class comarison {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		System.out.println(a>8);
+		
+		
+	}
+
+}
