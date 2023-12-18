b4cc24f10c09bf5c4b60400ce291330001b962c6
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2023-12-18 00:40:50 -0500

    string intro

---
+++ Practice/src/practice2/string.java
@@ -0,0 +1,27 @@
+package practice2;
+import java.util.Scanner;
+
+public class string {
+	public static void main(String[] args) {
+		
+		//String name = new String("CP");
+		//String name = "CP";
+		//System.out.print("The name is: ");
+		//System.out.println(name);
+		
+		//int a = 6;
+		//float b= 5.7654f;
+		
+		//System.out.printf("The value of a is %d and value of b is %f",a,b); 
+		
+		//System.out.format("The value of a is %d and value of b is %f",a,b);
+		
+		Scanner sc = new Scanner (System.in);
+		String st = sc.nextLine(); // this is used to take more than one words
+		System.out.println(st);
+		
+		
+		
+	}
+
+}
