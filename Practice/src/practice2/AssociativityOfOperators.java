6b9b8c71145259e9d4040c3f4e0b2943cddc7ea6
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2023-12-18 00:08:36 -0500

    assiciativity

---
+++ Practice/src/practice2/AssociativityOfOperators.java
@@ -0,0 +1,24 @@
+package practice2;
+
+public class AssociativityOfOperators {
+	public static void main(String[] args) {
+		
+		
+		int a = 6*5-34/2;
+		/*
+		 * =30-17 
+		 * =13  
+		 */
+		int b = 60/5-34*2;
+		/*
+		 * = 12-68
+		 * =-56
+		 */
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		
+	}
+
+}
