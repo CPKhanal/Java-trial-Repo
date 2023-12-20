93a61884823af6b5c58d21a7a3aa672ab405ab5a
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2023-12-20 01:53:24 -0500

    logical operators

--- Practice/src/practice2/operators.java
+++ Practice/src/practice2/operators.java
@@ -3,8 +3,29 @@ package practice2;
 public class operators {
 	public static void main(String[] args) {
 		
+		boolean a = true;
+		boolean b = false;
+		
+		System.out.println("This is for logical AND");
+		if(a && b) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
 		
 		
+		System.out.println("This is for logical OR");
+		if(a || b) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+		
+		
+		System.out.println("Not(a) is "+ !a);
+		System.out.println("Not(b) is "+ !b);
 		
 	}
 
