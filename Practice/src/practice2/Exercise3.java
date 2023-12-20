1dbf24c90abc357ca3fa72db21ce0033bd931c0d
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2023-12-20 00:12:39 -0500

    exercise from video 15 CWH

---
+++ Practice/src/practice2/Exercise3.java
@@ -0,0 +1,34 @@
+package practice2;
+
+public class Exercise3 {
+	public static void main(String[] args) {
+		
+		// Problem 1
+		/* String name = "Making All The Text To Lower Case";
+		name = name.toLowerCase();
+		System.out.println(name); */
+		
+		
+		// Problem 2
+		/* String text = "Changing Spaces With Underscore.";
+		System.out.println(text.replace(" ", "_")); */
+		
+		// Problem 3
+		/* String letter = "Dear <|name|>, Thanks a lot!";
+		letter = letter.replace("<|name|>", "CP");
+		System.out.println(letter);  */
+		
+		//Problem 4
+		/* String myString = " This sentence contains  double and  not any triple spaces";
+		System.out.println(myString.indexOf("  "));
+		System.out.println(myString.indexOf("   ")); */
+		
+		// Problem 5
+		String myLetter = ("Hello there, \nthis java couse is nice. \n\tThanks!");
+		System.out.println(myLetter);
+		
+		
+		
+	}
+
+}
