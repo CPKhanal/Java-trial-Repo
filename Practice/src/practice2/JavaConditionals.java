30250c5c811271bd5dfdf9b16660138805df6c91
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2023-12-20 00:55:09 -0500

    conditionals

---
+++ Practice/src/practice2/JavaConditionals.java
@@ -0,0 +1,16 @@
+package practice2;
+
+public class JavaConditionals {
+	public static void main(String[] args) {
+		
+		int age = 13;
+		if(age>18) {
+			System.out.println("Congratulations! You can drive");
+		}
+		
+		else {
+			System.out.println("Sorry! You can't drive");
+		}
+	}
+
+}
