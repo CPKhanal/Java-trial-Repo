357ad35188362579ee193dbe3f32b8cdb842e929
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2024-01-05 00:53:15 -0500

    Introduction to array

--- Practice/src/practice3/IntroToArray.java
+++ Practice/src/practice3/IntroToArray.java
@@ -1,5 +1,24 @@
 package practice3;
 
 public class IntroToArray {
+	public static void main(String[] args) {
+		//Array is a collection of similar types of data
+		
+		
+		/*int [] marks;
+		marks = new int[5]; */
+		
+		 /*int [] marks = new int[5];
+		marks [0] = 100;
+		marks [1] = 60;
+		marks [2] = 70;
+		marks [3] = 90;
+		marks [4] = 86;
+	System.out.println(marks[3]); */
+		
+		int [] marks = { 100, 90, 80, 70, 60};
+		System.out.println(marks[2]);
+		
+	}
 
 }
