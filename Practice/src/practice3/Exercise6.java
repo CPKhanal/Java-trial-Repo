42bf24578314c3067bfc2c3e5b71c886b86f33bc
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2024-01-08 01:07:24 -0500

    Exercise 6

---
+++ Practice/src/practice3/Exercise6.java
@@ -0,0 +1,51 @@
+package practice3;
+
+public class Exercise6 {
+	public static void main(String[] args) {
+		
+		// Problem 1
+		   /*float [] marks = {45.7f, 67.8f, 63.4f, 9.2f, 100f};
+		float sum =0;
+		
+		for (float element:marks) {
+			sum=sum+element;
+		}
+		System.out.println("The sum of marks is " + sum);*/
+		
+		
+		// Problem 2
+			/*	float [] marks = {45.7f, 67.8f, 63.4f, 9.2f, 100f};
+				float num = 45.7f;
+				boolean isInArray = false;
+				
+				for (float element:marks) {
+					if(num==element) {
+						isInArray = true;
+						break;
+					}
+				}
+				if(isInArray) {
+				System.out.println("The value is present");
+				}
+				else{
+					System.out.println("The vlaue is not present");
+				} */
+		
+		// problem 3
+		   /*  float [] marks = {45.7f, 67.8f, 63.4f, 9.2f, 100f};
+		float sum =0;
+		
+		for (float element:marks) {
+			sum=sum+element;
+		}
+		System.out.println("The average marks is " + sum/marks.length); */
+		
+		
+		// Problem 4
+		
+		
+				
+		
+				
+	} //Do not come across this
+}
